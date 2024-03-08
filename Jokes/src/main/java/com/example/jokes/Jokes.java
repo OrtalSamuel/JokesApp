2d983bb4a73e52c9d751c6a2fbb300be058daf34
OrtalSamuel <107999485+OrtalSamuel@users.noreply.github.com>
2024-03-08 14:28:21 +0200

    Joke Application

---
+++ Jokes/src/main/java/com/example/jokes/Jokes.java
@@ -0,0 +1,4 @@
+package com.example.jokes;
+
+public class Jokes {
+}
---
+++ Jokes/src/main/java/com/example/jokes/Retrofit/JokeApi.java
@@ -0,0 +1,13 @@
+package com.example.jokes.Retrofit;
+
+public interface JokeApi {
+ }
+
+//     @GET("/jokes/{category}")
+//     Call<String> getJokeByCategory(@Path("category") String category);
+//
+//     @POST("/jokes/add")
+//     Call<Joke> addJoke(@Body Joke joke);
+// }
+//
+//
